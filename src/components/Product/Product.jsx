8c5750c89e83c8bd6a0fbb85d1803ae953e8a774
tamzid620 <tamzid620@gmail.com>
2023-03-27 01:47:40 +0600

    sixth commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -17,7 +19,9 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings} Stars</p>
             </div>
-            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart
+            {/* <FontAwesomeIcon icon={faShoppingCart} /> */}
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
 
     useEffect( ()=>{
         fetch('products.json')
@@ -12,7 +13,9 @@ const Shop = () => {
     },[] )
 
     const handleAddToCart = (product)=> {
-        console.log(product)
+        // cart.push(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
 
     return (
@@ -28,6 +31,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
+                <p>Selected Items :{cart.length}</p>
             </div>
         </div>
     );
