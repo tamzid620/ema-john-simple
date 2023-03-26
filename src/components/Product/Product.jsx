f3aa0132b27e5288552680f484608086c401be63
tamzid620 <tamzid620@gmail.com>
2023-03-27 00:50:21 +0600

    fifth commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,11 @@ import './Product.css'
 
 const Product = (props) => {
     const {img, name, seller,ratings ,quantity, price}= props.product;
+    // const handleAddToCart = (product)=> {
+    //     console.log(product)
+    // }
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +17,7 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
