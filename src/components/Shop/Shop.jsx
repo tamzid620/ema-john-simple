109e0acd906f2ee180917f27e5fd4fe1cebccf3b
tamzid620 <tamzid620@gmail.com>
2023-03-26 22:10:12 +0600

    second commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>products here</h3>
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
