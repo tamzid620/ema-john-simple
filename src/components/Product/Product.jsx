06c53035099858960b61e452ffdb874249369681
tamzid620 <tamzid620@gmail.com>
2023-03-27 00:11:54 +0600

    forth commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price}= props.product;
+    const {img, name, seller,ratings ,quantity, price}= props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} Stars</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
