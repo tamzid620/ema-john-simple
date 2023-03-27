709e52e699b6b57e8149f105ea40cbad3da20a6b
tamzid620 <tamzid620@gmail.com>
2023-03-27 22:30:07 +0600

    eight commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,12 +12,17 @@ const Shop = () => {
         fetch('products.json')
         .then(res=> res.json())
         .then(data=> setProducts(data))
-    },[] )
+    },[] );
+
+    useEffect(() =>{
+        const storedCart = getShoppingCart()
+    },[])
 
     const handleAddToCart = (product)=> {
         // cart.push(product)
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
