6a3abacd48802d170fad9877d6c18cee14492072
tamzid620 <tamzid620@gmail.com>
2023-03-28 00:03:19 +0600

    eleventh commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,8 +8,15 @@ const Cart = ({cart}) => {
 let totalPrice = 0;
 let totalShipping = 0;
 for(const product of cart){
-    totalPrice = totalPrice + product.price;
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // }
+
+    // product.quantity = product.quantity || 1 ;
+
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    // quantity = quantity + product.quantity;
 }
 const tax = totalPrice *7/100;
 
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -35,7 +35,18 @@ const Shop = () => {
 
     const handleAddToCart = (product)=> {
         // cart.push(product)
-        const newCart = [...cart, product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists]
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
