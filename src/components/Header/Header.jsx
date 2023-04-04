302e0a8afc5ab4e1a1a5dc42afc8421cf40bbc5e
tamzid620 <tamzid620@gmail.com>
2023-04-04 22:07:26 +0600

    router installed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
-           <img src={logo} alt="" />
-           <div>
-            <a href="/Shop">Shop</a>
-            <a href="/Order">Order</a>
-            <a href="/Inventory">Inventory</a>
-            <a href="/Login">Login</a>
-           </div>
+            <img src={logo} alt="" />
+            <div>
+                <Link to="/Shop">Shop</Link>
+                <Link to="/Orders">Orders</Link>
+                <Link to="/Inventory">Inventory</Link>
+                <Link to="/Login">Login</Link>
+            </div>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children:[
+      {
+        path: "/",
+        element: <Shop></Shop>
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>
+      },
+      {
+        path: "Inventory",
+        element: <Inventory></Inventory>
+      },
+      {
+        path: "Login",
+        element: <Login></Login>
+      },
+    ]
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
